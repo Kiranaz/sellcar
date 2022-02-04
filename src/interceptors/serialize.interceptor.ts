67c8c21740ba658c0d75ecdaa6065a9f5787c1db
kiranaz <kiranaz_123@outlook.com>
2022-02-04 12:40:26 +0500

    Till section 10

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,14 +2,18 @@ import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from 
 import { Observable } from "rxjs";
 import { plainToClass } from "class-transformer";
 import { map } from "rxjs";
+import { argv } from "process";
 
+interface classConstructor {
+    new (...args: any[] ): {} //This type specifies a function that accepts any number of arguments that are any type, returns any value, and can be invoked with new.
+}
 
-export function Serialize(dto: any){
+export function Serialize(dto: classConstructor){
     return UseInterceptors(new SerializeInterceptor(dto))
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-    constructor(private dto: any) {}
+    constructor(private dto: classConstructor) {}
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
         //Run something before a request is handled by request handler
         //console.log('Running before the handler', context);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Patch, Delete, Param, Query, NotFoundException, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Delete, Param, Query, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -6,6 +6,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
@@ -16,7 +17,7 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @Serialize(UserDto)
+    
     //@UseInterceptors(ClassSerializerInterceptor) //Bad approach
     @Get('/:id')
     async findUser(@Param('id') id: string) { //we always get id as string from param so we need to parse into int when dealing with it
